bde47c78fae424b77c3c25c3f6cd28e7656b954a
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-26 22:54:53 +0530

    Added logic for finding the fibonacci series for given number

---
+++ src/com/java/logicalprograms/FibonacciSeries.java
@@ -0,0 +1,40 @@
+package com.java.logicalprograms;
+/**
+ * imports all the class of the java.util package
+ */
+import java.util.Scanner;
+
+public class FibonacciSeries
+{
+	/**
+	 * program execution starts from main method
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		/*
+		 * Taking user input using Scanner object
+		 */
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter the numbers");
+		int number = input.nextInt();
+		/*
+		 * Declaring the variables
+		 */
+		int firstTerm=0;
+		int secondTerm=1;
+		System.out.println("Fibonacci series"+number+" term");
+		/*
+		 * Logic for checking fibonacii series
+		 */
+		for(int i=1;i<=number;i++)
+		{
+			System.out.println(firstTerm+" ");
+			int nextTerm=firstTerm + secondTerm;
+			firstTerm=secondTerm;
+			secondTerm=nextTerm;
+		}
+	}
+	
+
+}
