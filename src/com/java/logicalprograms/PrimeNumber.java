3ae64484bacfe09a0abb27fd18d6f66b420c4ce9
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-26 22:29:25 +0530

    Added logic for finding the a given number is prime number

---
+++ src/com/java/logicalprograms/PrimeNumber.java
@@ -0,0 +1,39 @@
+package com.java.logicalprograms;
+/**
+ * imports the Scanner class of the java.util package
+ */
+import java.util.Scanner;
+
+public class PrimeNumber 
+{
+	/**
+	 * program execution starts from main method
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		/*
+		 * Taking user input using Scanner object
+		 */
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int number = input.nextInt();		
+	    boolean flag = false;
+	    for (int i = 2; i <= number / 2; i++) 
+	    {
+	      /*
+	       *  condition for Non prime number
+	       */
+	      if (number % i == 0) 
+	      {
+	        flag = true;
+	        break;
+	      }
+	    }
+	    if (!flag)
+	      System.out.println(number + " is a prime number.");
+	    else
+	      System.out.println(number + " is not a prime number.");
+	}
+
+}
